6d4b852b6d9af5859f848195e7346e91f0b9d69e
kaoskar1 <oscar.ljungdahl@hotmail.com>
2022-05-20 12:59:12 +0200

    type script store

--- src/store/user/user.selector.ts
+++ src/store/user/user.selector.ts
@@ -1,8 +1,11 @@
 import { createSelector } from "reselect"
 
+import 	{RootState} from '../store'
+
+
 import { UserState } from "./user.reducer"
 
-export const SelectUSerReducer = (state): UserState => state.user
+export const SelectUSerReducer = (state: RootState): UserState => state.user
 
 export const selectCurrentUser = createSelector(SelectUSerReducer, (user) => user.currentUser)
 
